843681b7dd710db3fd1292515ab49e5eadb602dc
Daryl <dtjh@live.com.sg>
2019-08-05 22:30:02 +1000

    json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/src'));
 
 app.get('*', function (req, res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + 'src/app/home/home.component.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
