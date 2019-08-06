4deb5e043aa66d791a37b8d1fc088419e9f60434
Daryl <dtjh@live.com.sg>
2019-08-06 15:39:09 +1000

    dist

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>' -------------- src is the app name
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/bounce'));
 
 app.get('*', function (req, res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + 'src/app/home/home.component.html'));
+    res.sendFile(path.join(__dirname + 'dist/bounce/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
