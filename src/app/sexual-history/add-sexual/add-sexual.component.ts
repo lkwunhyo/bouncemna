de5dcb94d410d7f019c473e063f519c412419e9b
Kevin Lee <38714085+lkwunhyo@users.noreply.github.com>
2019-09-11 15:57:19 +1000

    Session Storage and Add Partner in Add Sexual Feature

--- src/app/sexual-history/add-sexual/add-sexual.component.ts
+++ src/app/sexual-history/add-sexual/add-sexual.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebStorageService, SESSION_STORAGE } from 'angular-webstorage-service';
+import { Inject } from '@angular/core';  
 
 @Component({
   selector: 'app-add-sexual',
@@ -12,6 +14,8 @@ export class AddSexualComponent implements OnInit {
 
   activities_performed = [];
   contraceptives_used = [];
+
+  sexualpartners = [];
 
   OnCheckboxSelect(item, array, status:boolean) {
     if (array.indexOf(item) === -1 && status) {
@@ -31,9 +35,24 @@ export class AddSexualComponent implements OnInit {
   }
 
 
-  constructor() { }
+  constructor(@Inject(SESSION_STORAGE) private storage: WebStorageService) {
+
+  }
+
+  getFromSession() {
+    // Get from Session Storage
+    //this.sexualpartners = Object.values(this.storage);
+    var values = Object.values(this.storage);
+    //this.sexualpartners = Object.values(values[0]);
+    var keys = Object.keys(values[0]);
+    console.log(keys);
+
+    this.sexualpartners = keys.map(key => this.storage.get(key));
+    console.log(this.sexualpartners);
+  }
 
   ngOnInit() {
+    this.getFromSession();
   }
 
 }
