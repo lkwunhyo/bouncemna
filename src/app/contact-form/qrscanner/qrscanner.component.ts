3f30cfe99212f64af51850c8fa09522f2ea4ad37
Kevin Lee <38714085+lkwunhyo@users.noreply.github.com>
2019-10-21 13:51:30 +1000

    qrscanner fix without ssl

--- src/app/contact-form/qrscanner/qrscanner.component.ts
+++ src/app/contact-form/qrscanner/qrscanner.component.ts
@@ -12,14 +12,21 @@ import { ContactFormService } from '../../services/contact-form.service';
 export class QrscannerComponent implements OnInit {
 
   qrData = null;
+
   hasCameras = false;
+  hasDevices: boolean;
   hasPermission: boolean;
+
+  scannerEnabled = true;
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+
   contact = new Person();
   contactForm: FormGroup;
   errorMessage: string = null;
   errorCount: number = 0;
   
-  @ViewChild('scanner')
+  //@ViewChild('scanner')
   scanner: QrscannerComponent;
 
   constructor(private router: Router, private formBuilder: FormBuilder, private _contactFormService: ContactFormService) { 
@@ -30,36 +37,48 @@ export class QrscannerComponent implements OnInit {
 
   scanCode(data: any) {
     try {
-    console.log('Result: ', data);
-    this.qrData = JSON.parse(data);
-    console.log(this.qrData);
+      console.log('Result: ', data);
+      this.qrData = JSON.parse(data);
+      console.log(this.qrData);
 
-    this.contactForm = this.formBuilder.group({
-      //'id': this.contact.id,
-      firstname: this.qrData.firstname,
-      lastname: this.qrData.lastname,
-      phone: this.qrData.phone,
-      email: this.qrData.email,
-      age: this.qrData.age,
-      gender: this.qrData.gender,
-      comment: this.qrData.comment,
-      rating: this.qrData.rating
-      // some other stuff
-    });
+      this.contactForm = this.formBuilder.group({
+        //'id': this.contact.id,
+        firstname: this.qrData.firstname,
+        lastname: this.qrData.lastname,
+        phone: this.qrData.phone,
+        email: this.qrData.email,
+        age: this.qrData.age,
+        gender: this.qrData.gender,
+        comment: this.qrData.comment,
+        rating: this.qrData.rating
+        // some other stuff
+      });
 
-    console.log("Contact Form: " + JSON.stringify(this.contactForm.value));
-    this._contactFormService.addcontact(this.qrData).subscribe(
-      data => console.log('Success!', data),
-      error => console.error('Error!', error)
-      );
+      this.scannerEnabled = false;
+
+      console.log("Contact Form: " + JSON.stringify(this.contactForm.value));
+      this._contactFormService.addcontact(this.qrData).subscribe(
+        data => console.log('Success!', data),
+        error => console.error('Error!', error)
+        );
 
       var url = window.location.origin + "/contact";
       location.replace(url);
+
     } catch(e) {
       this.errorCount++;
       this.errorMessage = `Please scan a valid user QR Code. (${this.errorCount})`;
     }
     
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
   }
 
   ngOnInit() {
