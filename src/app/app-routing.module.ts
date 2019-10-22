5844c23a7eee067568182d693d3495801481740b
Daryl <dtjh@live.com.sg>
2019-10-23 01:02:23 +1000

    default route login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ import { QrscannerComponent } from './contact-form/qrscanner/qrscanner.component
 
 
 const routes: Routes = [
-{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: '', redirectTo: '/login', pathMatch: 'full'},
 { path: 'home', component: HomeComponent },
 { path: 'login', component: LoginComponent},
 { path: 'register', component: RegisterComponent},
